cd044f4ebe7870ba0a45621f7f7bff8a00d4a4f8
Zebb0a <156677733+Zebb0a@users.noreply.github.com>
2026-01-07 11:03:22 +0100

    Update script.js

--- lab/e-commerce_json_xml_csv/script.js
+++ lab/e-commerce_json_xml_csv/script.js
@@ -1,25 +1,25 @@
-var carrello = [];
+let carrello = [];
 
-//CSV
-function caricaCSV(callback) {
-  var richiesta = new XMLHttpRequest();
+// --- Caricamento CSV ---
+function caricaCSV(it) {
+  let richiesta = new XMLHttpRequest();
   richiesta.open("GET", "./prodotti/prodotti.csv", true);
   richiesta.onreadystatechange = function () {
     if (richiesta.readyState == 4) {
-      callback(richiesta.responseText);
+      it(richiesta.responseText);
     }
   };
   richiesta.send();
 }
 
-function convertiCSV(testoCSV) {
-  var righe = testoCSV.trim().split(/\r?\n/);
-  var intestazioni = righe[0].split(",");
-  var prodotti = [];
-  for (var i = 1; i < righe.length; i++) {
-    var valori = righe[i].split(",");
-    var prodotto = {};
-    for (var j = 0; j < intestazioni.length; j++) {
+function convertiCSV(CSV) {
+  let righe = CSV.trim().split(/\r?\n/);
+  let intestazioni = righe[0].split(",");
+  let prodotti = [];
+  for (let i = 1; i < righe.length; i++) {
+    let valori = righe[i].split(",");
+    let prodotto = {};
+    for (let j = 0; j < intestazioni.length; j++) {
       prodotto[intestazioni[j]] = (valori[j]).trim();
     }
     prodotti.push(prodotto);
@@ -27,61 +27,61 @@ function convertiCSV(testoCSV) {
   return prodotti;
 }
 
-//JSON
-function caricaJSON(callback) {
-  var richiesta = new XMLHttpRequest();
+// --- Caricamento JSON ---
+function caricaJSON(it) {
+  let richiesta = new XMLHttpRequest();
   richiesta.open("GET", "./prodotti/prodotti.json", true);
   richiesta.onreadystatechange = function () {
     if (richiesta.readyState == 4) {
-      callback(JSON.parse(richiesta.responseText));
+      it(JSON.parse(richiesta.responseText));
     }
   };
   richiesta.send();
 }
 
-//XML
-function caricaXML(callback) {
-  var richiesta = new XMLHttpRequest();
+// --- Caricamento XML ---
+function caricaXML(it) {
+  let richiesta = new XMLHttpRequest();
   richiesta.open("GET", "./prodotti/prodotti.xml", true);
   richiesta.onreadystatechange = function () {
     if (richiesta.readyState == 4) {
-      var documentoXML = richiesta.responseXML;
-      var elementi = documentoXML.getElementsByTagName("product");
-      var prodotti = [];
-      for (var i = 0; i < elementi.length; i++) {
-        var prodotto = {
-            codice: elementi[i].getElementsByTagName("id")[0].textContent,
-            titolo: elementi[i].getElementsByTagName("title")[0].textContent,
-            categoria: elementi[i].getElementsByTagName("type")[0].textContent,
-            prezzo: elementi[i].getElementsByTagName("price")[0].textContent,
-            immagine: elementi[i].getElementsByTagName("image")[0].textContent
+      let documentoXML = richiesta.responseXML;
+      let elementi = documentoXML.getElementsByTagName("product");
+      let prodotti = [];
+      for (let i = 0; i < elementi.length; i++) {
+        let prodotto = {
+          codice: elementi[i].getElementsByTagName("id")[0].textContent,
+          titolo: elementi[i].getElementsByTagName("title")[0].textContent,
+          categoria: elementi[i].getElementsByTagName("type")[0].textContent,
+          prezzo: elementi[i].getElementsByTagName("price")[0].textContent,
+          immagine: elementi[i].getElementsByTagName("image")[0].textContent
         };
         prodotti.push(prodotto);
       }
-      callback(prodotti);
+      it(prodotti);
     }
   };
   richiesta.send();
 }
 
-//card
+// --- Creazione card prodotto ---
 function creaCard(prodotto) {
-  var card = document.createElement("div");
+  let card = document.createElement("div");
   card.className = "card";
 
-  var immagine = document.createElement("img");
+  let immagine = document.createElement("img");
   immagine.src = prodotto.immagine || prodotto.image;
   card.appendChild(immagine);
 
-  var titolo = document.createElement("h3");
+  let titolo = document.createElement("h3");
   titolo.textContent = prodotto.titolo || prodotto.title;
   card.appendChild(titolo);
 
-  var prezzo = document.createElement("p");
+  let prezzo = document.createElement("p");
   prezzo.textContent = (prodotto.prezzo || prodotto.price) + " €";
   card.appendChild(prezzo);
 
-  var bottone = document.createElement("button");
+  let bottone = document.createElement("button");
   bottone.textContent = "Aggiungi al carrello";
   bottone.onclick = function () {
     carrello.push(prodotto);
@@ -92,25 +92,25 @@ function creaCard(prodotto) {
   return card;
 }
 
-//prod
+// --- Mostra prodotti ---
 function mostraProdotti(listaProdotti, idContenitore) {
-  var contenitore = document.getElementById(idContenitore);
+  let contenitore = document.getElementById(idContenitore);
   contenitore.innerHTML = "";
-  for (var i = 0; i < listaProdotti.length; i++) {
-    var card = creaCard(listaProdotti[i]);
+  for (let i = 0; i < listaProdotti.length; i++) {
+    let card = creaCard(listaProdotti[i]);
     contenitore.appendChild(card);
   }
 }
 
-//carrello
+// --- Aggiorna carrello ---
 function aggiornaCarrello() {
-  var lista = document.getElementById("lista-carrello");
-  var totaleEl = document.getElementById("totale");
+  let lista = document.getElementById("lista-carrello");
+  let totaleEl = document.getElementById("totale");
   lista.innerHTML = "";
-  var totale = 0;
+  let totale = 0;
 
-  for (var i = 0; i < carrello.length; i++) {
-    var item = document.createElement("p");
+  for (let i = 0; i < carrello.length; i++) {
+    let item = document.createElement("p");
     item.textContent = (carrello[i].titolo || carrello[i].title) + " - " + (carrello[i].prezzo || carrello[i].price) + " €";
     lista.appendChild(item);
     totale += parseFloat(carrello[i].prezzo || carrello[i].price);
@@ -124,21 +124,21 @@ function svuotaCarrello() {
   aggiornaCarrello();
 }
 
-//stamp
+// --- Stampa con window.print ---
 function stampaScontrino() {
-  var contenuto = "<h1>Scontrino</h1>";
-  var totale = 0;
+  let contenuto = "<h1>Scontrino</h1>";
+  let totale = 0;
 
-  for (var i = 0; i < carrello.length; i++) {
-    var titolo = carrello[i].titolo || carrello[i].title;
-    var prezzo = carrello[i].prezzo || carrello[i].price;
+  for (let i = 0; i < carrello.length; i++) {
+    let titolo = carrello[i].titolo || carrello[i].title;
+    let prezzo = carrello[i].prezzo || carrello[i].price;
     contenuto += "<p>" + titolo + " - " + prezzo + " €</p>";
     totale += parseFloat(prezzo);
   }
 
   contenuto += "<h3>Totale: " + totale.toFixed(2) + " €</h3>";
 
-  var finestraStampa = window.open("", "", "width=600,height=400");
+  let finestraStampa = window.open("", "", "width=600,height=400");
   finestraStampa.document.write("<html><head><title>Scontrino</title></head><body>");
   finestraStampa.document.write(contenuto);
   finestraStampa.document.write("</body></html>");
@@ -152,8 +152,8 @@ window.onload = function () {
     mostraProdotti(prodottiJSON, "json-container");
   });
 
-  caricaCSV(function (testoCSV) {
-    var prodottiCSV = convertiCSV(testoCSV);
+  caricaCSV(function (CSV) {
+    let prodottiCSV = convertiCSV(CSV);
     mostraProdotti(prodottiCSV, "csv-container");
   });
 
@@ -161,4 +161,3 @@ window.onload = function () {
     mostraProdotti(prodottiXML, "xml-container");
   });
 };
-
